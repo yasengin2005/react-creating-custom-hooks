60638c830bb5a4533796528829a155265633e2e8
Engin Büküm <yasengin2005@gmail.com>
2023-02-27 16:14:24 +0300

    react-custom-hooks-link

--- src/App.js
+++ src/App.js
@@ -3,11 +3,6 @@ import { useInput } from "./hooks/useInput";
 import "./styles.css";
 
 export default function App() {
-  // const [name, setName] = useState("");
-  // const [age, setAge] = useState("");
-  // const [email, setEmail] = useState("");
-
-  // console.log(name, age, email);
   const [inputs, setInputs] = useInput({
     name: "",
     age: "",
@@ -17,22 +12,30 @@ export default function App() {
 
   return (
     <div className="App">
-      <h1>Custom React Hook </h1>
-      <h2>How to write a custom hooks</h2>
+      <h1>React Hooks</h1>
+      <h2>Custom hooks</h2>
 
-      { /* prettier-ignore */}
       <form>
         <label htmlFor="name">
-        Name: <input name="name" value={inputs.name} onChange={setInputs} />
+          Name:
+          <input name="name" value={inputs.name} onChange={setInputs} />
         </label>
         <label htmlFor="age">
-        Age: <input name="age" value={inputs.age} onChange={setInputs} />
+          Age:
+          <input name="age" value={inputs.age} onChange={setInputs} />
         </label>
         <label htmlFor="email">
-        Email: <input name="email" value={inputs.email} onChange={setInputs} />
+          Email:
+          <input name="email" value={inputs.email} onChange={setInputs} />
         </label>
         <label htmlFor="password">
-        Password: <input name="password" type="password" value={inputs.password} onChange={setInputs} />
+          Password:
+          <input
+            name="password"
+            type="password"
+            value={inputs.password}
+            onChange={setInputs}
+          />
         </label>
       </form>
     </div>
--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -3,11 +3,11 @@ import { useState } from "react";
 export const useInput = (params) => {
   const [inputs, setInputs] = useState(params);
 
-  const handleChange = (event) => {
+  const changeHandler = (event) => {
     setInputs({ ...inputs, [event.target.name]: event.target.value });
   };
 
   console.log(inputs);
 
-  return [inputs, handleChange];
+  return [inputs, changeHandler];
 };
